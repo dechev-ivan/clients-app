114ca89f66a9a4ac175af138dcf476dc4728da58
Ivan Dechev <i.dechev@idaproject.com>
2025-06-11 07:37:27 +0300

    feat(main page): redirect to clients, error page

---
+++ error.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type {NuxtError} from '#app'
+import {UiTags} from "#layers/ui-kit/types/enum";
+
+defineProps({
+    error: Object as () => NuxtError
+})
+</script>
+
+<template>
+    <div :class="$style.ErrorPage">
+        <h1 :class="$style.title">{{ error?.statusCode }}</h1>
+        <UiButton
+            :tag="UiTags.RLink"
+            to="/"
+        >
+            Go back home
+        </UiButton>
+    </div>
+</template>
+
+<style module lang="scss">
+.ErrorPage {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    min-height: 100vh;
+    flex-direction: column;
+}
+
+.title {
+    margin-bottom: 40px;
+    font-size: 48px;
+}
+</style>
